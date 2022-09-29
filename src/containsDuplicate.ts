1662736701bd694040d27cb5cfcd271731f96a71
김용호 <yhkim@miridih.com>
2022-09-29 23:28:41 +0900

    2022.09.29: Contains Duplicate https://leetcode.com/problems/contains-duplicate/

---
+++ src/containsDuplicate.ts
@@ -0,0 +1,15 @@
+// Runtime: 193 ms, faster than 22.89% of TypeScript online submissions for Contains Duplicate.
+// Memory Usage: 52.4 MB, less than 7.50% of TypeScript online submissions for Contains Duplicate.
+export function containsDuplicate(nums: number[]): boolean {
+    const map: Record<string, number> = {};
+    for (const num of nums) {
+        const key = num.toString();
+        if (map[key] === undefined) {
+            map[key] = 1;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+};
