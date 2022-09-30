ca94d61943fa2cdbe62cd7f4d7deb0e16cf96d25
김용호 <yhkim@miridih.com>
2022-09-30 23:17:24 +0900

    fix: 기존 script 포맷 * containsDuplicate

--- src/containsDuplicate.test.ts
+++ src/containsDuplicate.test.ts
@@ -2,14 +2,14 @@ import { containsDuplicate } from './containsDuplicate'
 
 describe('sum func', () => {
     test('[1,2,3,1]', () => {
-        expect(containsDuplicate([1,2,3,1])).toBe(true)
+        expect(containsDuplicate([1, 2, 3, 1])).toBe(true)
     })
 
     test('[1,2,3,4]', () => {
-        expect(containsDuplicate([1,2,3,4])).toBe(false)
+        expect(containsDuplicate([1, 2, 3, 4])).toBe(false)
     })
 
     test('[1,1,1,3,3,4,3,2,4,2]', () => {
-        expect(containsDuplicate([1,1,1,3,3,4,3,2,4,2])).toBe(true)
+        expect(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])).toBe(true)
     })
 })
