1204307c1217d006e959762a5e7c6cd0f956e7d3
김용호 <yhkim@miridih.com>
2022-12-04 00:53:22 +0900

    2022.12.04: LinkedListCycle https://leetcode.com/problems/linked-list-cycle/

---
+++ src/141/LinkedListCycle.ts
@@ -0,0 +1,25 @@
+export class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val
+        this.next = next === undefined ? null : next
+    }
+}
+
+export function hasCycle(head: ListNode | null): boolean {
+    if (!head || !head.next) {
+        return false
+    }
+
+    while (head.next !== null) {
+        if (head.val === Infinity) {
+            return true
+        }
+
+        head.val = Infinity
+        head = head?.next
+    }
+
+    return false
+}
