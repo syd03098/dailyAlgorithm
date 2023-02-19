8ba71bc5e1405655813ba49fefc84e423b1c578b
김용호 <yhkim@miridih.com>
2023-02-20 01:22:15 +0900

    2022.02.20 minHeap
    * https://dev.to/taumechanica/generic-binary-heap-in-typescript-ilc

---
+++ src/algorithms/minHeap.test.ts
@@ -0,0 +1,32 @@
+import MinHeap from './minHeap'
+
+interface Node {
+    value: number
+}
+
+describe('minHeap', () => {
+    test('basic', () => {
+        /**
+         *          0
+         *      1        2
+         * 3        4 5     6
+         */
+        const minHeap = new MinHeap<Node>((a, b) => b.value - a.value)
+        minHeap.add({ value: 6 })
+        minHeap.add({ value: 5 })
+        minHeap.add({ value: 4 })
+        minHeap.add({ value: 3 })
+        minHeap.add({ value: 2 })
+        minHeap.add({ value: 1 })
+        minHeap.add({ value: 0 })
+
+        expect(minHeap.pop()).toEqual({ value: 0 })
+        expect(minHeap.pop()).toEqual({ value: 1 })
+        expect(minHeap.pop()).toEqual({ value: 2 })
+        expect(minHeap.pop()).toEqual({ value: 3 })
+        expect(minHeap.pop()).toEqual({ value: 4 })
+        expect(minHeap.pop()).toEqual({ value: 5 })
+        expect(minHeap.pop()).toEqual({ value: 6 })
+        expect(minHeap.pop()).toEqual(undefined)
+    })
+})
